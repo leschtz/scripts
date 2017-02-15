36ebf91a4a319913bf6b88cea401235ef2bf0ff7
Lukas Schütz <schuetz.lukas@sbg.at>
2017-02-16 00:26:09 +0100

    changed error message

--- mcu.sh
+++ mcu.sh
@@ -2,7 +2,7 @@ export amount_file=$(ls | sed -e s/[^0-9]//g | wc -l)
 export pre=$(printf %02d $amount_file)
 
 if [ -z $1 ]; then
-	echo "Enter a Coursename!"
+	echo "Enter the name of the course."
 	exit 1
 fi
 
